d9451aa2ee30c77fae04f4321049bb56b3446348
yong.teng <yong.teng@liangvi.com>
2023-11-28 13:04:58 +0800

    更换统计代码 key

--- src/js/stat.js
+++ src/js/stat.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?b3129c8eeabdeda36bdfcd785b28a76c";
+  hm.src = "https://hm.baidu.com/hm.js?f051829d9d4cd980433078a2cfe5af2b";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
