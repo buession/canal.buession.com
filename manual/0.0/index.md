e1dfc2079a84b7aca17cb02c7877a69af540b576
eduosi <eduosi@users.noreply.github.com>
2024-02-05 13:52:24 +0800

    【部署】：2024-02-05 13:52:24

---
+++ manual/0.0/index.md
@@ -0,0 +1,12 @@
+# API 参考手册
+
+
+Buession Canal API 包含以下目录：
+
+
+|  模块                  | 使用帮助                        | 手册                                                                                 |
+|  ----                 | ----                           | ----                                 										        |
+| com-canal-annotation  | [使用帮助](annotation/index.md) | [API 手册](https://javadoc.io/static/com.buession.canal/com-canal-annotation/0.0.1/)  |
+| com-canal-core        | [使用帮助](core/index.md)       | [API 手册](https://javadoc.io/static/com.buession.canal/com-canal-core/0.0.1/)        |
+| com-canal-client      | [使用帮助](client/index.md)     | [API 手册](https://javadoc.io/static/com.buession.canal/com-canal-client/0.0.1/)      |
+| com-canal-spring      | [使用帮助](spring/index.md)     | [API 手册](https://javadoc.io/static/com.buession.canal/com-canal-spring/0.0.1/)      |
---
+++ search_json.js
@@ -0,0 +1,212 @@
+window.ydoc_plugin_search_json = {
+  "文档": [
+    {
+      "title": "快速入门",
+      "content": "TIP\n\n官方指南假设您已了解\"JAVA\"、以及 spring、springboot 方面的相关知识。\n\nBuession Canal 是一款实现对阿里巴巴 canal 客户端封装类库。您可以根据本文档中的示例，快速熟悉 Buession Canal 的使用方法。",
+      "url": "/docs/quickstart.html",
+      "children": [
+        {
+          "title": "下一步可做什么？",
+          "url": "/docs/quickstart.html#下一步可做什么？",
+          "content": "下一步可做什么？您对 Buession Canal 大致了解后，您接下来可以做以下事情：了解兼容性：了解 Buession Canal 的兼容性\n安装：安装/引用 Buession Canal\n使用：开始使用 Buession Canal 功能\n"
+        }
+      ]
+    },
+    {
+      "title": "框架介绍",
+      "content": "",
+      "url": "/docs/intro.html",
+      "children": [
+        {
+          "title": "Buession Canal 框架是什么？",
+          "url": "/docs/intro.html#buession-canal-框架是什么？",
+          "content": "Buession Canal 框架是什么？Buession Canal 是一款实现对阿里巴巴 canal 客户端封装类库。"
+        }
+      ]
+    },
+    {
+      "title": "开源协议",
+      "content": "                             Apache License                       Version 2.0, January 2004\n                    http://www.apache.org/licenses/\nTERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION\nDefinitions.\n\"License\" shall mean the terms and conditions for use, reproduction,\nand distribution as defined by Sections 1 through 9 of this document.\n\"Licensor\" shall mean the copyright owner or entity authorized by\nthe copyright owner that is granting the License.\n\"Legal Entity\" shall mean the union of the acting entity and all\nother entities that control, are controlled by, or are under common\ncontrol with that entity. For the purposes of this definition,\n\"control\" means (i) the power, direct or indirect, to cause the\ndirection or management of such entity, whether by contract or\notherwise, or (ii) ownership of fifty percent (50%) or more of the\noutstanding shares, or (iii) beneficial ownership of such entity.\n\"You\" (or \"Your\") shall mean an individual or Legal Entity\nexercising permissions granted by this License.\n\"Source\" form shall mean the preferred form for making modifications,\nincluding but not limited to software source code, documentation\nsource, and configuration files.\n\"Object\" form shall mean any form resulting from mechanical\ntransformation or translation of a Source form, including but\nnot limited to compiled object code, generated documentation,\nand conversions to other media types.\n\"Work\" shall mean the work of authorship, whether in Source or\nObject form, made available under the License, as indicated by a\ncopyright notice that is included in or attached to the work\n(an example is provided in the Appendix below).\n\"Derivative Works\" shall mean any work, whether in Source or Object\nform, that is based on (or derived from) the Work and for which the\neditorial revisions, annotations, elaborations, or other modifications\nrepresent, as a whole, an original work of authorship. For the purposes\nof this License, Derivative Works shall not include works that remain\nseparable from, or merely link (or bind by name) to the interfaces of,\nthe Work and Derivative Works thereof.\n\"Contribution\" shall mean any work of authorship, including\nthe original version of the Work and any modifications or additions\nto that Work or Derivative Works thereof, that is intentionally\nsubmitted to Licensor for inclusion in the Work by the copyright owner\nor by an individual or Legal Entity authorized to submit on behalf of\nthe copyright owner. For the purposes of this definition, \"submitted\"\nmeans any form of electronic, verbal, or written communication sent\nto the Licensor or its representatives, including but not limited to\ncommunication on electronic mailing lists, source code control systems,\nand issue tracking systems that are managed by, or on behalf of, the\nLicensor for the purpose of discussing and improving the Work, but\nexcluding communication that is conspicuously marked or otherwise\ndesignated in writing by the copyright owner as \"Not a Contribution.\"\n\"Contributor\" shall mean Licensor and any individual or Legal Entity\non behalf of whom a Contribution has been received by Licensor and\nsubsequently incorporated within the Work.\n\n\nGrant of Copyright License. Subject to the terms and conditions of\nthis License, each Contributor hereby grants to You a perpetual,\nworldwide, non-exclusive, no-charge, royalty-free, irrevocable\ncopyright license to reproduce, prepare Derivative Works of,\npublicly display, publicly perform, sublicense, and distribute the\nWork and such Derivative Works in Source or Object form.\n\n\nGrant of Patent License. Subject to the terms and conditions of\nthis License, each Contributor hereby grants to You a perpetual,\nworldwide, non-exclusive, no-charge, royalty-free, irrevocable\n(except as stated in this section) patent license to make, have made,\nuse, offer to sell, sell, import, and otherwise transfer the Work,\nwhere such license applies only to those patent claims licensable\nby such Contributor that are necessarily infringed by their\nContribution(s) alone or by combination of their Contribution(s)\nwith the Work to which such Contribution(s) was submitted. If You\ninstitute patent litigation against any entity (including a\ncross-claim or counterclaim in a lawsuit) alleging that the Work\nor a Contribution incorporated within the Work constitutes direct\nor contributory patent infringement, then any patent licenses\ngranted to You under this License for that Work shall terminate\nas of the date such litigation is filed.\n\n\nRedistribution. You may reproduce and distribute copies of the\nWork or Derivative Works thereof in any medium, with or without\nmodifications, and in Source or Object form, provided that You\nmeet the following conditions:\n(a) You must give any other recipients of the Work or\nDerivative Works a copy of this License; and\n(b) You must cause any modified files to carry prominent notices\nstating that You changed the files; and\n(c) You must retain, in the Source form of any Derivative Works\nthat You distribute, all copyright, patent, trademark, and\nattribution notices from the Source form of the Work,\nexcluding those notices that do not pertain to any part of\nthe Derivative Works; and\n(d) If the Work includes a \"NOTICE\" text file as part of its\ndistribution, then any Derivative Works that You distribute must\ninclude a readable copy of the attribution notices contained\nwithin such NOTICE file, excluding those notices that do not\npertain to any part of the Derivative Works, in at least one\nof the following places: within a NOTICE text file distributed\nas part of the Derivative Works; within the Source form or\ndocumentation, if provided along with the Derivative Works; or,\nwithin a display generated by the Derivative Works, if and\nwherever such third-party notices normally appear. The contents\nof the NOTICE file are for informational purposes only and\ndo not modify the License. You may add Your own attribution\nnotices within Derivative Works that You distribute, alongside\nor as an addendum to the NOTICE text from the Work, provided\nthat such additional attribution notices cannot be construed\nas modifying the License.\nYou may add Your own copyright statement to Your modifications and\nmay provide additional or different license terms and conditions\nfor use, reproduction, or distribution of Your modifications, or\nfor any such Derivative Works as a whole, provided Your use,\nreproduction, and distribution of the Work otherwise complies with\nthe conditions stated in this License.\n\n\nSubmission of Contributions. Unless You explicitly state otherwise,\nany Contribution intentionally submitted for inclusion in the Work\nby You to the Licensor shall be under the terms and conditions of\nthis License, without any additional terms or conditions.\nNotwithstanding the above, nothing herein shall supersede or modify\nthe terms of any separate license agreement you may have executed\nwith Licensor regarding such Contributions.\n\n\nTrademarks. This License does not grant permission to use the trade\nnames, trademarks, service marks, or product names of the Licensor,\nexcept as required for reasonable and customary use in describing the\norigin of the Work and reproducing the content of the NOTICE file.\n\n\nDisclaimer of Warranty. Unless required by applicable law or\nagreed to in writing, Licensor provides the Work (and each\nContributor provides its Contributions) on an \"AS IS\" BASIS,\nWITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or\nimplied, including, without limitation, any warranties or conditions\nof TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A\nPARTICULAR PURPOSE. You are solely responsible for determining the\nappropriateness of using or redistributing the Work and assume any\nrisks associated with Your exercise of permissions under this License.\n\n\nLimitation of Liability. In no event and under no legal theory,\nwhether in tort (including negligence), contract, or otherwise,\nunless required by applicable law (such as deliberate and grossly\nnegligent acts) or agreed to in writing, shall any Contributor be\nliable to You for damages, including any direct, indirect, special,\nincidental, or consequential damages of any character arising as a\nresult of this License or out of the use or inability to use the\nWork (including but not limited to damages for loss of goodwill,\nwork stoppage, computer failure or malfunction, or any and all\nother commercial damages or losses), even if such Contributor\nhas been advised of the possibility of such damages.\n\n\nAccepting Warranty or Additional Liability. While redistributing\nthe Work or Derivative Works thereof, You may choose to offer,\nand charge a fee for, acceptance of support, warranty, indemnity,\nor other liability obligations and/or rights consistent with this\nLicense. However, in accepting such obligations, You may act only\non Your own behalf and on Your sole responsibility, not on behalf\nof any other Contributor, and only if You agree to indemnify,\ndefend, and hold each Contributor harmless for any liability\nincurred by, or claims asserted against, such Contributor by reason\nof your accepting any such warranty or additional liability.\n\nEND OF TERMS AND CONDITIONSAPPENDIX: How to apply the Apache License to your work.  To apply the Apache License to your work, attach the following  boilerplate notice, with the fields enclosed by brackets \"[]\"\n  replaced with your own identifying information. (Don't include\n  the brackets!)  The text should be enclosed in the appropriate\n  comment syntax for the file format. We also recommend that a\n  file or class name and description of purpose be included on the\n  same \"printed page\" as the copyright notice for easier\n  identification within third-party archives.\nCopyright [yyyy] [name of copyright owner]Licensed under the Apache License, Version 2.0 (the \"License\");you may not use this file except in compliance with the License.\nYou may obtain a copy of the License at   http://www.apache.org/licenses/LICENSE-2.0Unless required by applicable law or agreed to in writing, softwaredistributed under the License is distributed on an \"AS IS\" BASIS,\nWITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\nSee the License for the specific language governing permissions and\nlimitations under the License.",
+      "url": "/docs/license.html",
+      "children": []
+    },
+    {
+      "title": "模块说明",
+      "content": "",
+      "url": "/docs/module.html",
+      "children": [
+        {
+          "title": "buession-canal-annotation",
+          "url": "/docs/module.html#buession-canal-annotation",
+          "content": "buession-canal-annotation封装 buession canal 注解\n"
+        },
+        {
+          "title": "buession-canal-core",
+          "url": "/docs/module.html#buession-canal-core",
+          "content": "buession-canal-corebuession canal 核心库\n"
+        },
+        {
+          "title": "buession-canal-client",
+          "url": "/docs/module.html#buession-canal-client",
+          "content": "buession-canal-clientbuession canal 客户端\n"
+        },
+        {
+          "title": "buession-canal-spring",
+          "url": "/docs/module.html#buession-canal-spring",
+          "content": "buession-canal-springbuession canal springframework 支持\n"
+        }
+      ]
+    },
+    {
+      "title": "版本说明",
+      "content": "该项目基于 GNU 版风格定义项目版本，即：主版本号.子版本号.修正版本号。管理策略主版本号，发生变更时，不保证所有的 API 对上一个版本兼容，但保障大部分能兼容；主版本变更，可能涉及类、接口、枚举、方法的删除，或者包名的变更\n子版本号，发生变更时，完全兼容上一个版本，主要会增加一些小的功能或API，底层逻辑的调整调优\n修正版本号，主要用于修复 BUG、优化性能、安全漏洞修复，不会新增、变更、删除已有 API\n三方包兼容性说明当引用的三方包，我们保证尽大可能兼容。但对于 springframework、springboot、springcloud、springsecurity、springdata 等 spring 家族组件，以及 servlet 兼容对应的主版本。",
+      "url": "/docs/version.html",
+      "children": []
+    },
+    {
+      "title": "安装及使用",
+      "content": "",
+      "url": "/docs/installation.html",
+      "children": [
+        {
+          "title": "Maven 中央仓库搜索",
+          "url": "/docs/installation.html#maven-中央仓库搜索",
+          "content": "Maven 中央仓库搜索https://mvnrepository.com/search?q=com.buession.canal\nhttps://search.maven.org/search?q=g:com.buession.canal\n"
+        },
+        {
+          "title": "手动编译",
+          "url": "/docs/installation.html#手动编译",
+          "content": "手动编译git clone https://github.com/buession/buession-canalcd buession-canal/buession-canal-parent && mvn clean install\n"
+        },
+        {
+          "title": "Maven",
+          "url": "/docs/installation.html#maven",
+          "content": "Maven    com.buession.canal\n    buession-canal-xxx\n    x.x.x\n\n"
+        },
+        {
+          "title": "Gradle",
+          "url": "/docs/installation.html#gradle",
+          "content": "Gradlecompile group: 'com.buession.canal', name: 'buession-canal-xxx', version: 'x.x.x'其中，artifactId 中的 xxx 表示对应的子模块；version 中的 x.x.x 代表版本号，根据需要使用特定版本，建议使用 maven 仓库中已构建好的最新版本的包。"
+        }
+      ]
+    },
+    {
+      "title": "环境要求",
+      "content": "",
+      "url": "/docs/requirement.html",
+      "children": [
+        {
+          "title": "环境要求",
+          "url": "/docs/requirement.html#环境要求",
+          "content": "环境要求JDKJDK 8+构建工具\n\n构建工具\n版本\n\n\n\n\nMaven\n3.5+\n\n\nGradle\n6.x+，推荐 6.3 及以上版本\n\n\nServlet 容器支持 servlet 3.1+，推荐使用 servlet 4.0 及以上版本。"
+        }
+      ]
+    }
+  ],
+  "参考手册": [
+    {
+      "title": "参考手册简介",
+      "content": "Buession Canal 是一款实现对阿里巴巴 canal 客户端封装类库。",
+      "url": "/manual/index.html",
+      "children": []
+    },
+    {
+      "title": "参考指南",
+      "content": "本文档包含了完整的 Buession Canal 的参考文档。\n\n版本\n手册\n\n\n\n\n0.0.x\nAPI 手册\n\n\n",
+      "url": "/manual/overview.html",
+      "children": []
+    },
+    {
+      "title": "buession-canal-annotation 参考手册",
+      "content": "buession canal 注解。",
+      "url": "/manual/0.0/annotation/index.html",
+      "children": [
+        {
+          "title": "安装",
+          "url": "/manual/0.0/annotation/index.html#安装",
+          "content": "安装    com.buession.canal\n    buession-canal-annotation\n    x.x.x\n\n该模块主要定义 buession canal 注解。"
+        },
+        {
+          "title": "API 参考手册>>",
+          "url": "/manual/0.0/annotation/index.html#api-参考手册>>",
+          "content": "API 参考手册>>"
+        }
+      ]
+    },
+    {
+      "title": "buession-canal-annotation 参考手册",
+      "content": "",
+      "url": "/manual/0.0/annotation/annotation.html",
+      "children": [
+        {
+          "title": "注解",
+          "url": "/manual/0.0/annotation/annotation.html#注解",
+          "content": "注解\n\n注解\n说明\n\n\n\n\n@CanalBinding\ncanal 执行类绑定\n\n\n@CanalEventListener\n数据库操作监听器\n\n\n@AlertTableEventListener\n修改数据表操作监听器\n\n\n@CreateIndexEventListener\n创建索引操作监听器\n\n\n@CreateTableEventListener\n创建数据表操作监听器\n\n\n@DeleteEventListener\n删除数据（DELETE）操作监听器\n\n\n@DropIndexEventListener\n删除索引操作监听器\n\n\n@DropTableEventListener\n删除数据表操作监听器\n\n\n@InsertEventListener\n插入数据（INSERT）操作监听器\n\n\n@RenameTableEventListener\n重命名数据表操作监听器\n\n\n@TruncateTableEventListener\n截断数据表操作监听器\n\n\n@UpdateEventListener\n更新数据（UPDATE）操作监听器\n\n\n@Destination\n指令注解，用于监听器方法获取当前指令\n\n\n@Row\n监听器方法获取当前操作行数据\n\n\n@Schema\n监听器方法获取当前数据库\n\n\n@Table\n听器方法获取当前数据表\n\n\n"
+        }
+      ]
+    },
+    {
+      "title": "buession-canal-core 参考手册",
+      "content": "buession canal 核心类库。",
+      "url": "/manual/0.0/core/index.html",
+      "children": [
+        {
+          "title": "安装",
+          "url": "/manual/0.0/core/index.html#安装",
+          "content": "安装    com.buession.canal\n    buession-canal-core\n    x.x.x\n\n该模块定义了 buession canal 核心 API。"
+        },
+        {
+          "title": "API 参考手册>>",
+          "url": "/manual/0.0/core/index.html#api-参考手册>>",
+          "content": "API 参考手册>>"
+        }
+      ]
+    },
+    {
+      "title": "buession-canal-client 参考手册",
+      "content": "buession canal 客户端。",
+      "url": "/manual/0.0/client/index.html",
+      "children": [
+        {
+          "title": "安装",
+          "url": "/manual/0.0/client/index.html#安装",
+          "content": "安装    com.buession.canal\n    buession-canal-client\n    x.x.x\n\n该模块主要实现了 tcp、kafka、RabbitMQ、Pulsar、RocketMQ 适配器客户端，以及 buession canal 客户端 API。"
+        },
+        {
+          "title": "API 参考手册>>",
+          "url": "/manual/0.0/client/index.html#api-参考手册>>",
+          "content": "API 参考手册>>"
+        }
+      ]
+    },
+    {
+      "title": "buession-springboot-captcha 参考手册",
+      "content": "",
+      "url": "/manual/0.0/client/ConfigurationProperties.html",
+      "children": [
+        {
+          "title": "配置属性",
+          "url": "/manual/0.0/client/ConfigurationProperties.html#配置属性",
+          "content": "配置属性通用\n\n属性\n类型\n默认值\n说明\n\n\n\n\nspring.captcha.javascript\nString[]\n--\n前端 JavaScript 库地址\n\n\n阿里云\n\n属性\n类型\n默认值\n说明\n\n\n\n\nspring.captcha.aliyun.enabled\nboolean\ntrue\n是否启用阿里云行为验证码\n\n\nspring.captcha.aliyun.access-key-id\nString\n--\nAccessKey ID\n\n\nspring.captcha.aliyun.access-key-secret\nString\n--\nAccessKey Secret\n\n\nspring.captcha.aliyun.app-key\nString\n--\n服务使用的 App Key\n\n\nspring.captcha.aliyun.region-id\nString\n--\n区域 ID\n\n\nspring.captcha.aliyun.parameter\nAliyunParameter\nAliyunParameter 默认值\n前端提交参数名称\n\n\n极验\n\n属性\n类型\n默认值\n说明\n\n\n\n\nspring.captcha.geetest.enabled\nboolean\ntrue\n是否启极验行为验证码\n\n\nspring.captcha.geetest.app-id\nString\n--\n应用 ID\n\n\nspring.captcha.geetest.secret-key\nString\n--\n密钥\n\n\nspring.captcha.geetest.version\nString\nv4\n版本\n\n\nspring.captcha.geetest.v3.parameter\nGeetestV3Parameter\nGeetestV3Parameter 默认值\n前端提交参数名称\n\n\nspring.captcha.geetest.v4.parameter\nGeetestV4Parameter\nGeetestV4Parameter 默认值\n前端提交参数名称\n\n\n腾讯\n\n属性\n类型\n默认值\n说明\n\n\n\n\nspring.captcha.tencent.enabled\nboolean\ntrue\n是否启腾讯云行为验证码\n\n\nspring.captcha.tencent.app-id\nString\n--\n应用 ID\n\n\nspring.captcha.tencent.secret-key\nString\n--\n密钥\n\n\nspring.captcha.tencent.parameter\nTencentParameter\nTencentParameter 默认值\n前端提交参数名称\n\n\n"
+        }
+      ]
+    },
+    {
+      "title": "buession-canal-spring 参考手册",
+      "content": "buession canal springframework 支持。",
+      "url": "/manual/0.0/spring/index.html",
+      "children": [
+        {
+          "title": "安装",
+          "url": "/manual/0.0/spring/index.html#安装",
+          "content": "安装    com.buession.canal\n    buession-canal-spring\n    x.x.x\n\n该模块主要为对 springframework 支持。"
+        },
+        {
+          "title": "API 参考手册>>",
+          "url": "/manual/0.0/spring/index.html#api-参考手册>>",
+          "content": "API 参考手册>>"
+        }
+      ]
+    }
+  ]
+}
